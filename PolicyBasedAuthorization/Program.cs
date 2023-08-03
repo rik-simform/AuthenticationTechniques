db139e1de34fe0765fc94795033e4204143ffc90
SF-LAP-874\rikesh <rikesh.b@simformsolutions.com>
2023-08-03 17:36:01 +0530

    Policy Based Authorization Demo

---
+++ PolicyBasedAuthorization/Program.cs
@@ -0,0 +1,48 @@
+using PolicyBasedAuthorization.Policies.Handlers;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using PolicyBasedAuthorization.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+{
+    options.Cookie.Name = ".AspNetCore.Cookies";
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+    options.SlidingExpiration = true;
+    options.LoginPath = "/Account/Login";
+    options.AccessDeniedPath =  "/Account/AccessDenied";
+
+});
+builder.Services.AddAuthorization(options =>
+{
+    //if you use this policy then your salary should equal or more than the 30000
+    options.AddPolicy("AtLeast30K", policy => policy.Requirements.Add(new SalaryRequirement(30000)));
+});
+builder.Services.AddSingleton<IAuthorizationHandler, SalaryHandler>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
